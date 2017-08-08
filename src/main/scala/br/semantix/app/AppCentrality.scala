7fa35ca79eb705bc88c6d52e3d1a6399144d8e0b
danilopds <danilopds@gmail.com>
2017-08-08 14:02:09 -0300

    First test-evaluation of closeness centrality

--- src/main/scala/br/semantix/app/AppCentrality.scala
+++ src/main/scala/br/semantix/app/AppCentrality.scala
@@ -4,10 +4,7 @@ import br.semantix.handlers.SparkHandler
 import br.semantix.settings.Settings
 import org.apache.spark.SparkContext
 import br.semantix.settings.Settings.{appName, edgesGraph}
-import ml.sparkling.graph.api.loaders.GraphLoading.LoadGraph
 import ml.sparkling.graph.api.operators.measures.VertexMeasureConfiguration
-import ml.sparkling.graph.loaders.csv.GraphFromCsv.CSV
-import ml.sparkling.graph.loaders.csv.GraphFromCsv.LoaderParameters.Delimiter
 import org.apache.spark.graphx.{Edge, Graph}
 import org.apache.spark.rdd.RDD
 import ml.sparkling.graph.operators.OperatorsDSL._
@@ -21,18 +18,17 @@ object AppCentrality {
   def main(args: Array[String]): Unit = {
     System.setProperty("hadoop.home.dir", Settings.winUtils)
 
-    val edges: RDD[Edge[String]] =
+    val edges: RDD[Edge[Int]] =
       sc.textFile(edgesGraph).map { line =>
         val fields = line.split(" ")
         Edge(fields(0).toLong, fields(1).toLong)
       }
 
-    val graph : Graph[Any, String] = Graph.fromEdges(edges, "defaultProperty")
+    val graph : Graph[Any, Int] = Graph.fromEdges(edges, "defaultProperty")
     println("num edges = " + graph.numEdges)
     println("num vertices = " + graph.numVertices)
 
-    /*val centralityGraph: Graph[Double, _] = graph.closenessCentrality(VertexMeasureConfiguration(treatAsUndirected=true))
-    println("num edges = " + centralityGraph.edges)
-    println("num vertices = " + centralityGraph.vertices)*/
+    val centralityGraph: Graph[Double, _] = graph.closenessCentrality()
+    centralityGraph.vertices.foreach(println)
   }
 }
